2fb370d65beb953b9def6072bd6465d89e2d5056
MaciekTalaska <maciek.talaska@gmail.com>
2018-09-01 15:18:39 +0200

    extract function refactored

--- app/Main.hs
+++ app/Main.hs
@@ -4,8 +4,12 @@ import Lib
 import Data.List
 import Data.Maybe
 
+findIndexToSplit :: (a -> Bool) -> [a] -> Int
+findIndexToSplit p line = fromMaybe (-1) (findIndex p line) +1
+
 extract :: [Char] -> [Char]
-extract line = snd (splitAt ((fromMaybe (-1) (findIndex (=='\t') line )) + 1) line)
+extract line = snd (splitAt (findIndexToSplit (=='\t') line) line)
+-- extract line = snd (splitAt ((fromMaybe (-1) (findIndex (=='\t') line )) + 1) line)
  
 main :: IO ()
 main = do
