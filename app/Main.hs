94b98881ac3727e7015016b2ce497bf99a4ea268
MaciekTalaska <maciek.talaska@gmail.com>
2018-09-01 16:05:14 +0200

    select random word from list

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import Lib
 import Data.List
 import Data.Maybe
+import System.Random
 
 findIndexToSplit :: (a -> Bool) -> [a] -> Int
 findIndexToSplit p line = fromMaybe (-1) (findIndex p line) +1
@@ -12,12 +13,15 @@ extract line = snd (splitAt (findIndexToSplit (=='\t') line) line)
 
 createRepository :: [[Char]] -> [[Char]]
 createRepository ls = map extract ls
- 
+
+getRandomElement :: [b] -> IO b
+getRandomElement ls = (randomRIO (0, length ls)) >>= \x -> return $ ls !! x
+
 main :: IO ()
 main = do
   contents <- readFile "diceware-en.txt"
   let ls = lines contents
-  -- let first_word = extract (head ls)
   let ws = createRepository ls
-  print "first 10 words: "
-  print (take 10 ws)
+  word <- getRandomElement ws
+  print "random word: "
+  print word
