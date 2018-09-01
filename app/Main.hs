271ae521b7fe7fa4ba9aabcf459028d20d999bb1
MaciekTalaska <maciek.talaska@gmail.com>
2018-09-01 15:10:29 +0200

    extract word from line containing index and word

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,16 @@
 module Main where
 
 import Lib
+import Data.List
+import Data.Maybe
 
+extract :: [Char] -> [Char]
+extract line = snd (splitAt ((fromMaybe (-1) (findIndex (=='\t') line )) + 1) line)
+ 
 main :: IO ()
 main = do
   contents <- readFile "diceware-en.txt"
   let ls = lines contents
-  print (take 10 ls)
+  let first_word = extract (head ls)
+  print "first word: "
+  print first_word 
