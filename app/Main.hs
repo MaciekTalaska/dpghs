73d357110ccfa1253273edec8166da2f15efeb45
MaciekTalaska <maciek.talaska@gmail.com>
2018-09-02 19:16:56 +0200

    first attempt to generating passwords

--- app/Main.hs
+++ app/Main.hs
@@ -25,14 +25,21 @@ getRandomElement2 list = do
   index <- (getRandomIndex list)
   return (list !! index)
 
+printMessageAndValue title password = do
+  print title
+  print password
+
 main :: IO ()
 main = do
   contents <- readFile "diceware-en.txt"
   let ls = lines contents
   let ws = createRepository ls
-  word <- getRandomElement ws
-  print "random word: "
-  print word
-  word2 <- getRandomElement2 ws 
-  print "random word2: "
-  print word2
+  --word <- getRandomElement ws
+  --printMessageAndValue "random word: " word
+  --word2 <- getRandomElement2 ws 
+  --printMessageAndValue "second word: " word2
+  password <- mapM (\_ -> getRandomElement ws) [1..5]
+  printMessageAndValue "password generated using mapM & getRandomElement: " password
+  password2 <- mapM (\_ -> getRandomElement2 ws) [1..5]
+  printMessageAndValue "password generated using mapM & getRandomElement2: " password2
+
