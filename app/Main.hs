bfb7ba8f8e28c6196619ea7555d120b0951357ed
MaciekTalaska <maciek.talaska@gmail.com>
2018-09-05 20:35:09 +0200

    enumerate diceware lists and generate passwords

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,14 @@
 module Main where
 
 import Utils
+import System.Directory
+import Data.List
+
+isDicewareFile :: [Char] -> Bool
+isDicewareFile filename = (isPrefixOf "diceware-" filename) && (isSuffixOf ".txt" filename)
 
 main :: IO ()
 main = do
-  printPassword "diceware-en.txt" 5
-  printPassword "diceware-pl.txt" 5
-  printPassword "diceware-fi.txt" 5
-  printPassword "diceware-mi.txt" 5
+  all_files <- getDirectoryContents "."
+  let diceware_files = (filter (\x -> isDicewareFile x) all_files)
+  mapM_ (\x -> printPassword x 5) diceware_files
