5a1f52bf51fe9e077adbd5e6ff9b0238cab2b63a
MaciekTalaska <maciek.talaska@gmail.com>
2018-09-10 19:26:27 +0200

    cleanup

--- app/Main.hs
+++ app/Main.hs
@@ -31,11 +31,3 @@ main = do
     otherwise -> do
       putStrLn "error! type: 'dpghs -h' for help"
       exitFail
-
---  diceware_files <- getDicewareFiles
---  passwords <- mapM (\x -> createPasswordFromDicewareList x 5 ) diceware_files
---  passwords2 <- mapM (\x -> createPasswordAsStringFromDicewareList2 x 5) diceware_files
---  print "passwords: "
---  print passwords
---  print "second batch of passwords: "
---  print passwords2
